d6fe623657a882cf8894c50c8166f80db2d4ed95
achf01 <annachiarafortuna01@gmail.com>
2023-02-15 15:53:48 +0100

    new spwan

---
+++ locosim/robot_control/vision/scripts/SpawnBlocks_temp.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+from gazebo_msgs.msg import *
+from gazebo_msgs.srv import *
+from geometry_msgs.msg import Pose, Point, Quaternion
+from pprint import pprint
+import random
+from math import pi as pi
+from math import cos as cos
+from math import sin as sin
+from math import atan2 as atan2
+import time
+import os
+
+np.set_printoptions(precision=5, suppress=True)
+block_num=2
+name=""
+
+
+block_names = ['X1-Y1-Z2', 'X1-Y2-Z1', 'X1-Y2-Z2', 'X1-Y2-Z2-CHAMFER', 'X1-Y2-Z2-TWINFILLET', 'X1-Y3-Z2', 'X1-Y3-Z2-FILLET',
+               'X1-Y4-Z2', 'X1-Y4-Z1', 'X2-Y2-Z2', 'X2-Y2-Z2-FILLET']
+blocks_info = {block_names[0]: (0.03, 0.03), block_names[1]: (0.03, 0.03), block_names[2]: (3, 6), block_names[3]: (0.03, 0.06),
+               block_names[4]: (0.03, 0.06), block_names[5]: (0.03, 0.09), block_names[6]: (3, 9), block_names[7]: (0.03, 0.12),
+               block_names[8]: (0.03, 0.12), block_names[9]: (0.06, 0.06), block_names[10]: (0.06, 0.06)}
+blocks_to_spawn = {'brick1': block_names[block_num]}
+''', 'block2': block_names[block_num], 'block3': block_names[block_num], 'block4': block_names[block_num],
+                   'block5': block_names[block_num], 'block6': block_names[block_num], 'block7': block_names[block_num], 'block8': block_names[block_num],
+                   'block9': block_names[block_num]}#, block10': block_names[9], 'block11': block_names[10]}'''
+
+class Block:
+    def __init__(self, name, position, orientation):
+        self.name = name
+        self.position = position
+        self.orientation = orientation
+        self.rotation = np.round(atan2(orientation.z, orientation.w), 5) * 2
+        block_type = blocks_to_spawn[name]
+        l, L = blocks_info[block_type]
+        span_x = np.round(L*sin(self.rotation) + l*cos(self.rotation), 5) * 1.5
+        span_y = np.round(L*cos(self.rotation) + l*sin(self.rotation), 5) * 1.5
+        self.span_x = [(position.x - span_x/2), (position.x + span_x/2)]
+        self.span_y = [(position.y - span_y/2), (position.y + span_y/2)]
+
+
+    def __str__(self):
+        return '---- Block info ----- \n' + str(self.name) + '\nposition:\n' + str(self.position) + '\n' + str(self.span_x) \
+               + ' - ' + str(self.span_y) + '\norientation:\n' + str(self.orientation) + '\nrotation: ' + str(self.rotation) + '\n---------------------'
+
+    def __repr__(self):
+        return str(self)
+
+
+def namedef(cl, posx, posy, r, p, y):
+    global name
+    name=""
+    name=str(cl)+"_"+str(posx)+"_"+str(posy)+"_"+str(r)+"_"+str(p)+"_"+str(y)+".jpg"
+    print("name:" +name+ "\n")
+
+
+def spawnBlocks():
+    spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+
+    for block in blocks_to_spawn:
+        model_name = block
+        spawn_model_client(
+            model_name=model_name,
+            model_xml=open('/home/annachiara/ros_ws/src/locosim/ros_impedance_controller/worlds/models/'+str(blocks_to_spawn[model_name])+'/model.sdf', 'r').read(),
+            robot_namespace='',
+            initial_pose=Pose(position=Point(0.5, 0.35, 0.925), orientation=Quaternion(0, 0, 0, 0)),
+            reference_frame='world'
+        )
+
+
+def getBlocksInfo():
+    # gazebo service: /gazebo/get_world_properties "{}"
+    rospy.wait_for_service('/gazebo/get_world_properties')
+    gwp = rospy.ServiceProxy('/gazebo/get_world_properties', GetWorldProperties)
+    res = gwp.call()
+
+    present_blocks = []
+    for model in res.model_names:
+        print(model)
+        if model in blocks_to_spawn:
+            present_blocks.append(model)
+
+    # gazebo service: /gazebo/get_model_properties nome_del_model
+    rospy.wait_for_service('/gazebo/get_model_state')
+    gms = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+
+    blocks = []
+    for block in present_blocks:
+        model_state = gms.call(block, 'link')
+        b = Block(block, model_state.pose.position, model_state.pose.orientation)
+        blocks.append(b)
+
+    return blocks
+
+
+def createQuaternion(iter):
+
+    #BLOCCO 1
+    if(iter<72):
+        pitch= 0
+        yaw=int(iter/9)*(pi/8)
+        roll= 0
+    elif(iter<144):
+        pitch=pi
+        yaw=int((iter%72)/9)*(pi/8)
+        roll= 0
+    elif(iter<288):
+        pitch=pi/2
+        roll= 0
+        yaw=int((iter%144)/9)*(pi/8)
+    else:
+        pitch=0
+        roll= pi/2
+        yaw=int((iter%288)/9)*(pi/8)
+    
+    
+
+    angles = [roll, pitch, yaw]
+
+    w = cos(angles[0]/2)*cos(angles[1]/2)*cos(angles[2]/2) + sin(angles[0]/2)*sin(angles[1]/2)*sin(angles[2]/2)
+    x = sin(angles[0]/2)*cos(angles[1]/2)*cos(angles[2]/2) - cos(angles[0]/2)*sin(angles[1]/2)*sin(angles[2]/2)
+    y = cos(angles[0]/2)*sin(angles[1]/2)*cos(angles[2]/2) + sin(angles[0]/2)*cos(angles[1]/2)*sin(angles[2]/2)
+    z = cos(angles[0]/2)*cos(angles[1]/2)*sin(angles[2]/2) - sin(angles[0]/2)*sin(angles[1]/2)*cos(angles[2]/2)
+
+    
+    return x, y, z, w, roll, pitch, yaw
+
+
+def moveBlock(blocks, iter):
+    print('moving blocks')
+
+    rospy.wait_for_service('/gazebo/set_model_state')
+    set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+
+    x_in=[0.25,0.5, 0.75]
+    y_in=[0.3125,0.475, 0.6375]
+    i=int(iter/3)%3
+    j=iter%3
+
+    for block in blocks:
+        ms = ModelState()
+        ms.model_name = block.name
+
+        x, y, z, w, roll, pitch, yaw= createQuaternion(iter)
+
+        ms.pose.orientation.x = x
+        ms.pose.orientation.y = y
+        ms.pose.orientation.z = z
+        ms.pose.orientation.w = w
+
+
+        
+        x = np.round(x_in[i], 5)
+        y = np.round(y_in[j], 5)
+        ms.pose.position.x = x
+        ms.pose.position.y = y
+        
+
+        ms.pose.position.z = 0.925
+        print(ms)
+        namedef(block_num, x, y, roll, pitch, yaw)
+        print(set_state.call(ms))
+
+    print('blocks moved')
+
+
+def talker():
+    rospy.init_node('block_spawner_node', anonymous=True)
+
+    rate = rospy.Rate(500)
+
+    spawnBlocks()
+    i=0
+    while not rospy.is_shutdown():
+        #input("Press Enter to continue...")
+        blocks = getBlocksInfo()
+        pprint(blocks)
+        moveBlock(blocks, i)
+        i=i+1
+        if(i==433):
+            exit(0)
+        else:
+            time.sleep(5.)
+            os.system("python3 take_photo_temp.py "+ name)
+
+        time.sleep(2.)
+
+
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
---
+++ locosim/robot_control/vision/scripts/take_photo_temp.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import rospy
+from cv_bridge import CvBridge
+import os
+from datetime import datetime
+import cv2
+from sensor_msgs.msg import Image
+import sys
+import ast
+import time
+
+path_yolo = os.path.join(os.path.expanduser("~"), "ros_ws", "src", "locosim", "robot_control", "vision", "scripts",
+                        "yolov5")
+path_template = os.path.join(os.path.expanduser("~"), "ros_ws", "src", "locosim", "robot_control", "vision", "scripts",
+                            "template")
+nb="2"
+
+def detection(ar):
+    arg=ar
+    print("------------------------running yolo----------------------------")
+    os.system(
+        "python3 " + path_yolo + "/detect.py --weights " + path_yolo + "/best2.pt --source " + os.path.join(os.path.expanduser("~"), "yolo5_images", nb, arg)+ " --data " + path_yolo + "/data.yaml")
+    print("post detection") #DA CAMBIARE
+    with open(os.path.join(os.path.expanduser("~"), "ros_ws", "src", "locosim", "robot_control", "vision", "scripts",
+                        "yolov5", "res_save.txt")) as file:
+        det_res = [ast.literal_eval(line.rstrip("\n")) for line in file]
+    
+    if len(det_res):
+        xmin, ymin, xmax, ymax, p,c=det_res[0]
+        img=cv2.imread(os.path.join(os.path.expanduser("~"), "yolo5_images",nb, arg)) #DA CAMBIARE
+        img=img[ymin:ymax, xmin:xmax]
+        cv2.imwrite(os.path.join(os.path.expanduser("~"), "template",nb, arg), img) #DA CAMBIARE
+
+
+
+
+def retImageCallback(img, arg):
+
+    bridge = CvBridge()
+    cv_image = bridge.imgmsg_to_cv2(img, "mono8")
+    now = datetime.now()
+    t = arg
+    print(t)
+    cv2.imwrite(os.path.join(os.path.expanduser("~"), "yolo5_images",nb, t), cv_image) #DA CAMBIARE
+
+    sub.unregister()
+
+
+def talker(arg):
+    rospy.init_node('take_photo_node', anonymous=False)
+    print (arg) 
+    print (type(arg))
+    #img = rospy.wait_for_message('/ur5/zed2/left/image_rect_color', Image)
+    global sub
+    sub = rospy.Subscriber("/ur5/zed2/left/image_rect_color", Image, retImageCallback, callback_args=(arg),  queue_size=2)
+
+    #rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        for arg in range (1,len(sys.argv)):
+            print (sys.argv[arg]) 
+            print (type(sys.argv[arg]))
+            talker(sys.argv[arg])
+            detection(sys.argv[arg])
+
+    except rospy.ROSInterruptException:
+        pass
+
