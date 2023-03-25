a3d20fd409fecbbdeab6f1ebfff784c7310beb1a
achf01 <annachiarafortuna01@gmail.com>
2023-03-25 20:34:00 +0100

    blocco 0 sistemato

--- locosim/robot_control/vision/scripts/template/crop.py
+++ locosim/robot_control/vision/scripts/template/crop.py
@@ -2,11 +2,11 @@ import cv2
 import numpy as np
 
 
-img=cv2.imread("/home/annachiara/template/6/to_crop4.jpg", 0)
+img=cv2.imread("./0/to_crop.jpg", 0)
 print(img.shape)
 #img=img[482:535, 945:975]
 #img=img[475:535, 845:900]
 #img=img[480:530, 825:868]
 #img=img[480:530, 1050:1096]
-img=img[70:133, 389:412]
-cv2.imwrite("/home/annachiara/template/6/6_0.75_0.475_4.71238898038469_0_4.71238898038469.jpg", img)
+img=img[78:110, 385:414]
+cv2.imwrite("./0/0_0.8333_0.475_1.5708_0_1.9635.jpg", img)
