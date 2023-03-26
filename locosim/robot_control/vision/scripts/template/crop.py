a9827f92af1d8b2df3f3bd3564525f9903343343
achf01 <annachiarafortuna01@gmail.com>
2023-03-26 22:41:07 +0200

    blocco 3 nuovo

--- locosim/robot_control/vision/scripts/template/crop.py
+++ locosim/robot_control/vision/scripts/template/crop.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 
 
-img=cv2.imread("./2/to_crop.jpg", 0)
+img=cv2.imread("./3/to_crop1.jpg", 0)
 print(img.shape)
 #img=img[482:535, 945:975]
 #img=img[475:535, 845:900]
 #img=img[480:530, 825:868]
 #img=img[480:530, 1050:1096]
-img=img[333:425, 380:420]
-cv2.imwrite("./2/2_0.1667_0.475_0_3.14159_1.5708.jpg", img)
+img=img[360:425, 380:420]
+cv2.imwrite("./3/3_0.1667_0.475_1.5708_0_1.5708.jpg", img)
 
